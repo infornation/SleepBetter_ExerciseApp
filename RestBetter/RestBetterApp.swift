26c4e34f4d04c612e93a5a4b02002832e8d521a4
Matt Mauler <matt@metasonic.studio>
2022-09-20 01:01:09 +0200

    Initial Commit

---
+++ RestBetter/RestBetterApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestBetterApp.swift
+//  RestBetter
+//
+//  Created by Matěj Mauler on 20.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct RestBetterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
